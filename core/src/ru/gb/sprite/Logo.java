5d2a49045cfccf0ec9950a12cfeea4063ec4fb1f
yajaneya <janeya@yandex.ru>
2021-08-05 13:29:23 +0300

    Доработка класса Logo. Добавлена реализация движения к месту нажатия мыши (тачпада).

--- core/src/ru/gb/sprite/Logo.java
+++ core/src/ru/gb/sprite/Logo.java
@@ -1,6 +1,7 @@
 package ru.gb.sprite;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
@@ -12,8 +13,15 @@ public class Logo extends Sprite {
     private static final float SIZE_LOGO = 0.1f;
     private static final String ICON_LOGO = "badlogic.jpg";
 
+    private static final float V_STEP = 0.03f;
+
+    private Vector2 v;
+    private Vector2 touch;
+
     public Logo () {
         super(new TextureRegion(new Texture(ICON_LOGO)));
+        v = new Vector2();
+        touch = new Vector2();
     }
 
     @Override
@@ -23,9 +31,28 @@ public class Logo extends Sprite {
     }
 
     @Override
+    public void draw(SpriteBatch batch) {
+        System.out.println("Speed: " + v.x + ", " + v.y);
+        if (pos.dst(touch) > V_STEP) {
+            pos.add(v);
+        } else {
+            pos.set(touch);
+        }
+        super.draw(batch);
+    }
+
+    @Override
     public boolean touchDown(Vector2 touch, int pointer, int button) {
         System.out.println("Logo mouse");
-        pos.set(touch);
+        this.touch = touch;
+        v = touch.cpy().sub(pos).scl(V_STEP);
+
+
+        System.out.println("Position: " + pos.x + ", " + pos.y);
+        System.out.println("Purpose: " + touch.x + ", " + touch.y);
+        System.out.println("Speed: " + v.x + ", " + v.y);
+
+//        pos.set(touch);
         return false;
     }
 
